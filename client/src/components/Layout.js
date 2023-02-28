b72f6f0ad3b2e230bed40bf2d222f1cb538f5f3d
Tanaya <bhidetm@gmail.com>
2023-02-28 21:17:36 +0530

    icon hide success

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,6 +1,4 @@
-/*import React from "react";
-import "../Layout.css";
-import { useState } from "react";
+/*import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { RiCloseCircleFill, RiNotificationLine } from "react-icons/ri";
 import {
@@ -12,63 +10,48 @@ import {
 } from "react-icons/ri";
 import { useSelector } from "react-redux";
 
+import "../Layout.css";
+
 function Layout({ children }) {
   const [collapsed, setCollapsed] = useState(false);
-  const userData = useSelector(state => state.userData);
-  const { user } = userData || {}; // add a check to make sure userData is defined
+  const { user } = useSelector((state) => state.userData) || {};
   const location = useLocation();
+
   const userMenu = [
-    {
-      name: "Home",
-      path: "/",
-      icon: RiHome2Fill,
-    },
+    { name: "Home", path: "/", icon: RiHome2Fill },
     { name: "Appoint", path: "/appointments", icon: RiFile2Fill },
     { name: "Apply doctor", path: "/apply-doctor", icon: RiHospitalFill },
     { name: "Profile", path: "/profile", icon: RiProfileFill },
     { name: "Logout", path: "/logout", icon: RiLogoutBoxFill },
   ];
-
-  const menuItems = collapsed ? (
-    <div className="menu-collapsed">
-      {userMenu.map((menu) => {
-        const Icon = menu.icon;
-        return (
-          <Link to={menu.path}>
-            <div className="menu-item-collapsed">
+ 
+  const menuItems = userMenu.map((menu, index) => {
+    const isactive = location.pathname === menu.path;
+    const Icon = menu.icon;
+    return (
+      <Link key={index} to={menu.path}>
+        <div className={`d-flex menu-item ${isactive && "active-menu-item"}`}>
+          {collapsed ? (
+            <Icon className="ri-icon" />
+          ) : (
+            <div className="d-flex">
               <Icon className="ri-icon" />
+              <span>{menu.name}</span>
             </div>
-          </Link>
-        );
-      })}
-    </div>
-  ) : (
-    <div className="menu">
-      {userMenu.map((menu) => {
-        const isactive = location.pathname === menu.path;
-        const Icon = menu.icon;
-        return (
-          <Link to={menu.path}>
-            <div
-              className={`d-flex menu-item ${isactive && "active-menu-item"}`}
-            >
-              <Icon className="ri-icon" />
-              {menu.name}
-            </div>
-          </Link>
-        );
-      })}
-    </div>
-  );
+          )}
+        </div>
+      </Link>
+    );
+  });
 
   return (
     <div className="main">
       <div className="d-flex layout">
-        <div className={`${collapsed ? "collapsed-sidebar" : "sidebar"}`}>
+        <div className={`sidebar ${collapsed ? "collapsed" : ""}`}>
           <div className="sidebar-header">
             <h3>SEVA</h3>
           </div>
-          {menuItems}
+          <div className="menu">{menuItems}</div>
         </div>
         <div className="content">
           <div className="header">
@@ -77,9 +60,8 @@ function Layout({ children }) {
               onClick={() => setCollapsed(!collapsed)}
             />
             <div className="d-flex">
-              {" "}
               <RiNotificationLine className="layout-action-icon" />
-              <Link  className="anchor">{user?.name}</Link>
+              {user && <Link to="/profile">{user.name}</Link>}
             </div>
           </div>
           <div className="body">{children}</div>
@@ -89,8 +71,7 @@ function Layout({ children }) {
   );
 }
 
-export default Layout;
-*/
+export default Layout;*/
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { RiCloseCircleFill, RiNotificationLine } from "react-icons/ri";
@@ -124,8 +105,14 @@ function Layout({ children }) {
     return (
       <Link key={index} to={menu.path}>
         <div className={`d-flex menu-item ${isactive && "active-menu-item"}`}>
-          <Icon className="ri-icon" />
-          {menu.name}
+          {collapsed ? (
+            <Icon className="ri-icon" />
+          ) : (
+            <div className="d-flex">
+              <Icon className="ri-icon" />
+              <span>{menu.name}</span>
+            </div>
+          )}
         </div>
       </Link>
     );
